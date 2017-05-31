a1aca29ca55dd82d92dc6661730a975fdedbabf1
mabafaba <barner.mar@gmail.com>
2017-05-31 15:44:05 +0100

    dynamic popup content. hide popup on c

--- scripts.js
+++ scripts.js
@@ -4,12 +4,12 @@
 function init(){
 
 
-$( document ).ready( function() {
+
   // Scripts go here
   $('li').click(function(event){
-    $('.selected').css({
-      display: 'block'
-    })
+    // activate the popup and pass it the id of the icon that was clicked.
+    // the activatePopUp function uses the id to produce the image, pdf and html urls dynamically.
+    activatePopUp(this.id);
   });
 
   $('.body').click(function(){
@@ -26,20 +26,42 @@ $( document ).ready( function() {
 
 
 
-});
-
-
-
-
 $(document).mouseup(function (e) {
-     var popup = $("#popup");
-     if (!$('#open').is(e.target) && !popup.is(e.target) && popup.has(e.target).length == 0) {
-         popup.hide(500);
+     var popup = $(".selected");
+     if (!popup.is(e.target) && popup.has(e.target).length == 0 // if the click didnt target the popup..
+      && $(popup).css("display")!='none' // AND if the popup visibility was not set to 'none'
+      ) {
+     hidePopUp(); // then hide the popup!
      }
+  
+console.log()
  });
 
 
 }
 
+
+
+
+function iconClicked(iconID){
+
+}
+
+function activatePopUp(iconID){
+  // change popup image and links based on icon id:
+  $('.big-img').attr("src","images_web/"+iconID+"icon.gif");
+  $('#popup-online-link').attr("href",iconID+".html");
+  $('#popup-download-link').attr("href",iconID+".pdf");
+  // make popup visible:
+  $('.selected').css({
+      display: 'block'
+    })
+}
+function hidePopUp(iconID){
+  // change popup image and links based on icon id:
+  $('.selected').css({
+      display: 'none'
+    })
+}
 
 
